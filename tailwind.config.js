794d93fe52d683c38baaa0a798024b23f42cf0fa
amodkala <amodkala@gmail.com>
2024-08-08 17:30:22 -0400

    converted pages to MDX

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx,md,mdx}",
     ],
     theme: {
         extend: {},
@@ -12,4 +12,3 @@ export default {
         preflight: false,
     },
 }
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      mdx(),
+      react(),
+  ],
 })
