b0e946380a7032bab1b1f270c0a0efe2ec563875
amodkala <amodkala@gmail.com>
2024-09-09 21:47:47 -0400

    two new LC problems, syntax highlighting for code blocks

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react-swc'
+import rehypeHighlight from 'rehype-highlight'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      mdx(),
-      react(),
-  ],
+    plugins: [
+        mdx({
+            rehypePlugins: [
+                [rehypeHighlight, { detect: true, ignoreMissing: true }]
+            ]
+        }),
+        react(),
+    ],
 })
