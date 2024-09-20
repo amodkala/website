b26a0b835f3dc0e7ad8c08c3173d61913daa791c
amodkala <amodkala@gmail.com>
2024-09-19 21:18:32 -0400

    more leetcode prep, added page titles

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,12 +1,16 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 
 export default function Page() {
 
     const { Content } = useLoaderData();
+    const location = useLocation();
 
     return (
         <div>
+            <head>
+                <title>{location.pathname}</title>
+            </head>
             <Navbar />
             <Content />
         </div>
