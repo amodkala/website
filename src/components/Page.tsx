6a5425a5430bcfbafec2e21f3fa52704f7bf6203
amodkala <amodkala@gmail.com>
2024-09-24 14:29:02 -0400

    updates

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,12 +4,14 @@ import Navbar from "./Navbar";
 export default function Page() {
 
     const { Content } = useLoaderData();
-    const location = useLocation();
+    const path = useLocation().pathname.split("/").pop();
+    const suffix = path.length === 0 ? "" : " :: " + path;
+    const title = "amod kala" + suffix;
 
     return (
         <div>
             <head>
-                <title>{location.pathname}</title>
+                <title>{title}</title>
             </head>
             <Navbar />
             <Content />
