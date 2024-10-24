7688714e1a5286237fab8653f89c7009a03001ed
amodkala <amodkala@gmail.com>
2024-10-24 12:57:58 -0400

    content updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
     return (
         <div>
-            <ul className="pl-0">
+            <ul className="px-0">
                 <li className="inline pr-1.5">
                     <NavLink to="/">home</NavLink>
                 </li>
